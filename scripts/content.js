ad3312c13bc11db4220bab6ea1e6d376cfd9d958
Dchole <derekoware47@gmail.com>
2022-03-13 14:34:01 +0000

    first commit

---
+++ scripts/content.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", () => {
+  const allPageLinks = document.querySelectorAll("a")
+
+  const externalLinks = Array.from(allPageLinks).filter(
+    link => link.origin !== window.location.origin
+  )
+
+  externalLinks.forEach(link => {
+    console.log({ link })
+    if (link.target !== "_blank") {
+      link.classList.add("target-links")
+    }
+
+    link.classList.add("external-links")
+  })
+})
